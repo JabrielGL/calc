3171d080300b5b3b998c382868a40eeaa9067da4
Gabriel <tutogames730@gmail.com>
2018-10-04 22:44:13 -0300

    Calculadora financeira funcionando

--- venv/juros.py
+++ venv/juros.py
@@ -1,3 +1,4 @@
+from math import *
 def __init__(self, valor, tempo, fValor, perc):
     self.valor = valor
     self.tempo = tempo
@@ -6,10 +7,42 @@ def __init__(self, valor, tempo, fValor, perc):
 
 
 def JurosSimples(valor, perc, tempo):
-    fValor =valor+( (valor * (perc/100)) * tempo)
-    return fValor
+    fValor =valor + ((valor * (perc/100)) * tempo)
+    return round(fValor,2)
 
 def JurosComposto(valor, perc, tempo):
-    fValor = ((valor * (perc/100))**tempo)
-    return fValor
+    perc = 1+(perc/100)
+    fValor = valor*(perc**tempo)
+    return round(fValor,2)
 
+
+def SimplesInicial(fValor,perc,tempo):
+    perc = perc/100
+    valor = fValor/(1+(perc*tempo))
+    return round(valor,2)
+
+
+def CompostoInicial(fValor, perc, tempo):
+    perc =1+ perc /100
+    valor = fValor/((perc**tempo))
+    return round(valor,2)
+
+def SimplesTempo(fValor, valor, perc):
+    perc = perc/100
+    tempo = (fValor-valor)/(valor*perc)
+    return round(tempo,2)
+
+def CompostoTempo(fValor, valor, perc):
+    perc=perc/100
+    tempo = log(fValor/valor)/log(1+perc)
+    return round(tempo, 2)
+
+def SimplesJuros(fValor,valor,tempo):
+    perc = (fValor-valor)/(valor*tempo)
+    perc = perc*100
+    return round(perc, 2)
+
+def CompostoJuros(fValor,valor,tempo):
+    perc = ((fValor/valor)**(1 / tempo)) - 1
+    perc = perc*100
+    return round(perc,2)
--- venv/principal.py
+++ venv/principal.py
@@ -2,11 +2,38 @@ from tkinter import *
 import juros
 
 def comando():
-    result.delete(0,END)
-    if v == "simples":
-        result.insert(0,juros.JurosSimples(float(valor.get()),float(tempo.get()), float(perc.get())))
-    else:
-        result.insert(0,juros.JurosComposto(float(valor.get()),float(tempo.get()), float(perc.get())))
+
+    if v.get() == "Simples":
+        if valor.get() =="" and result.get()!="" and perc.get()!="" and tempo.get()!="":
+            valor.delete(0,END)
+            valor.insert(0, juros.SimplesInicial(float(result.get()), float(perc.get()), float(tempo.get())))
+        elif tempo.get()=="" and result.get()!="" and perc.get()!="" and valor.get()!="":
+            tempo.delete(0,END)
+            tempo.insert(0, juros.SimplesTempo(float(result.get()), float(valor.get()) ,float(perc.get())))
+        elif perc.get()=="" and result.get()!="" and tempo.get()!="" and valor.get()!="":
+            perc.delete(0,END)
+            perc.insert(0, juros.SimplesJuros(float(result.get()), float(valor.get()) ,float(tempo.get())))
+        elif result.get() =="" and valor.get()!="" and perc.get()!="" and tempo.get()!="":
+            result.delete(0, END)
+            result.insert(0, juros.JurosSimples(float(valor.get()), float(perc.get()), float(tempo.get())))
+        else:
+            toplevel = Toplevel()
+            Label(toplevel, text="Por favor, informe 3 campos!").pack()
+    elif v.get() == "Composto":
+        if valor.get() =="" and result.get()!="" and perc.get()!="" and tempo.get()!="":
+            valor.delete(0, END)
+            valor.insert(0, juros.CompostoInicial(float(result.get()), float(perc.get()), float(tempo.get())))
+        elif tempo.get() == "" and result.get() != "" and perc.get() != "" and valor.get() != "":
+            tempo.delete(0, END)
+            tempo.insert(0, juros.CompostoTempo(float(result.get()), float(valor.get()), float(perc.get())))
+        elif perc.get() == "" and result.get() != "" and tempo.get() != "" and valor.get() != "":
+            perc.delete(0,END)
+            perc.insert(0, juros.CompostoJuros(float(result.get()), float(valor.get()) ,float(tempo.get())))
+        elif result.get() == "" and valor.get() != "" and perc.get() != "" and tempo.get() != "":
+            result.insert(0, juros.JurosComposto(float(valor.get()), float(perc.get()), float(tempo.get())))
+        else:
+            toplevel = Toplevel()
+            Label(toplevel, text="Por favor, informe 3 campos!").pack()
 
 j = Tk()
 j.geometry('300x400')
@@ -24,9 +51,9 @@ Label(j, text="Resultado").place(x=50,y=200)
 result = Entry(j, fg='#FF8C00')
 result.place(x=130,y=200)
 v = StringVar()
-simples = Radiobutton(j, text="Simples", variable=v, value="simples").place(x=70, y=250)
-composto = Radiobutton(j, text="Composto", variable=v, value="composto").place(x=140,y=250)
-v.set('simples')
+simples = Radiobutton(j, text="Simples", variable=v, value="Simples").place(x=70, y=250)
+composto = Radiobutton(j, text="Composto", variable=v, value="Composto").place(x=140,y=250)
+v.set("Simples")
 
 Button(j, text="Calcular", command=comando).place(x=100,y=300)
 
